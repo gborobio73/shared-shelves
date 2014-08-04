c91924ffe0d3d91baeab71cc7930fa01d42f5ed3
gborobio73 <gborobio@gmail.com>
2014-08-04 19:40:46 +0300

    logout

---
+++ shared-shelves-war/src/main/java/com/leeloo/tbe/servlet/LogoutServlet.java
@@ -0,0 +1,31 @@
+package com.leeloo.tbe.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+public class LogoutServlet extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		Cookie acsid = new Cookie("ACSID", "");
+		acsid.setMaxAge(0);
+        resp.addCookie(acsid);
+        
+        Cookie sacsid = new Cookie("ACSID", "");
+        sacsid.setMaxAge(0);
+        resp.addCookie(sacsid);
+        
+        Cookie langCookie = new Cookie("ss_lang", "");
+        langCookie.setMaxAge(0);
+        resp.addCookie(langCookie);
+        
+        resp.sendRedirect("/login.html");   
+	}
+
+}
